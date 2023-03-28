5d31f037410580a5804b1c9574a7da88d1a5f442
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-28 01:03:32 +0100

    fix: modify

--- src/services/birthDayService.js
+++ src/services/birthDayService.js
@@ -18,7 +18,7 @@ const data = [
     name: 'Larry Little',
     age: 36,
     image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
   {
     id: 4,
