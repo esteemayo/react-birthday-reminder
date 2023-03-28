ce641e21c0bb7953cb4a4626d1b496bf3b8ac7bf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-28 01:02:52 +0100

    fix: modify

--- src/services/birthDayService.js
+++ src/services/birthDayService.js
@@ -1,7 +1,7 @@
 const data = [
   {
     id: 1,
-    name: "Bertie Yates",
+    name: 'Bertie Yates',
     age: 29,
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
